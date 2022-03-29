789c6eed338e6e9623fe37f6b330c772b5e9c8fb
heheyas <916973364@qq.com>
2022-03-29 13:15:50 +0800

    modify mdp

--- src/overcooked_ai_py/mdp/overcooked_mdp.py
+++ src/overcooked_ai_py/mdp/overcooked_mdp.py
@@ -1167,9 +1167,10 @@ class OvercookedGridworld(object):
 
             elif terrain_type == 'P' and not player.has_object():
                 # Cooking soup
-                if self.soup_to_be_cooked_at_location(new_state, i_pos):
-                    soup = new_state.get_object(i_pos)
-                    soup.begin_cooking()
+                # if self.soup_to_be_cooked_at_location(new_state, i_pos):
+                #     soup = new_state.get_object(i_pos)
+                #     soup.begin_cooking()
+                pass
             
             elif terrain_type == 'P' and player.has_object():
 
@@ -1201,6 +1202,10 @@ class OvercookedGridworld(object):
                         self.log_object_potting(events_infos, new_state, old_soup, soup, obj.name, player_idx)
                         if obj.name == Recipe.ONION:
                             events_infos['potting_onion'][player_idx] = True
+                        
+                    elif self.soup_to_be_cooked_at_location(new_state, i_pos):
+                        soup = new_state.get_object(i_pos)
+                        soup.begin_cooking()
 
             elif terrain_type == 'S' and player.has_object():
                 obj = player.get_object()
